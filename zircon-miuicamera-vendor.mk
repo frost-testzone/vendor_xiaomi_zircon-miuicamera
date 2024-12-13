a67987bafe334ead6b4ce374c71ef3f687b2ea7f
AdarshGrewal <adarshgrewal@gmail.com>
2024-12-13 20:57:41 +0530

    zircon-miuicamera: Import from zircon OS1.0.7.0.UNOMIXM

---
+++ zircon-miuicamera-vendor.mk
@@ -0,0 +1,18 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/zircon-miuicamera
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/zircon-miuicamera/proprietary/system/etc/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_ispinterface_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmtkisp_metadata_sys \
+    vendor.mediatek.hardware.camera.isphal-V1-ndk \
+    vendor.mediatek.hardware.camera.isphal@1.0_system_ext \
+    MiuiCamera
